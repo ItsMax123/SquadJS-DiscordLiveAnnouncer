370fc54740d453dbbf470c69b0360fc5f6bf2ee4
Max <maxime.mirorefice@gmail.com>
2025-05-11 18:21:56 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,62 @@
+# Discord Live Announcer
+
+A SquadJS plugin that send a discord message when the server goes live from seeding.
+
+## Installation
+
+1. Place `discord-live-announcer.js` in your plugins directory (`SquadJS/squad-server/plugins`).
+2. Add the configuration below to your SquadJS config file and edit it as you like.
+3. Restart SquadJS.
+
+## Config
+
+```json
+{
+  "plugin": "DiscordLiveAnnouncer",
+  "enabled": true,
+  "discordClient": "discord",
+  "channelID": "CHANNEL_ID",
+  "seedingThreshold": 50,
+  "content": "<@&1041503735660163112>",
+  "embed_title": "Server is LIVE",
+  "embed_url": null,
+  "embed_description": "Thank you to all who helped seed the server. Now come join the fun!",
+  "embed_color": 65280,
+  "embed_author_name": "DiscordLiveAnnouncer",
+  "embed_author_url": "https://github.com/ItsMax123/SquadJS-DiscordLiveAnnouncer",
+  "embed_author_icon_url": "https://picsum.photos/1080",
+  "embed_image_url": "https://picsum.photos/1080",
+  "embed_thumbnail_url": "https://picsum.photos/1080",
+  "embed_footer_text": "DiscordLiveAnnouncer",
+  "embed_footer_icon_url": "https://picsum.photos/1080",
+  "embed_fields_server": true,
+  "embed_fields_map": true,
+  "embed_fields_player_count": true,
+  "embed_fields_player_list": true
+}
+```
+
+Replace `CHANNEL_ID` with your Discord Channel ID.
+You can get this by having discord's Developer Mode turned on, right-clicking the channel you want the messages to be
+sent in and selecting the 'Copy Channel ID' option.
+
+| Field                     | Required | Description                                                           | Example                                                                 | Default |
+|---------------------------|----------|-----------------------------------------------------------------------|-------------------------------------------------------------------------|---------|
+| channelID                 | ✅        | The ID of the channel to send the announcements to.                   | `"1283199362318602270"`                                                 | N/A     |
+| seedingThreshold          | ✅        | Player count required for the server to be considered live.           | `50`                                                                    | N/A     |
+| content                   | ❌        | The content of the discord message to send when the server goes live. | `"<@&1041503735660163112>"`                                             | `null`  |
+| embed_title               | ❌        | The embed's title.                                                    | `"Server is LIVE"`                                                      | `null`  |
+| embed_url                 | ❌        | The embed's url.                                                      | `"https://github.com/ItsMax123/SquadJS-DiscordLiveAnnouncer"`           | `null`  |
+| embed_description         | ❌        | The embed's description.                                              | `"Thank you to all who helped seed the server. Now come join the fun!"` | `null`  |
+| embed_color               | ❌        | The embed's color.                                                    | `65280`                                                                 | `65280` |
+| embed_author_name         | ❌        | The embed's author's name.                                            | `"DiscordLiveAnnouncer"`                                                | `null`  |
+| embed_author_url          | ❌        | The embed's author's url.                                             | `"https://github.com/ItsMax123/SquadJS-DiscordLiveAnnouncer"`           | `null`  |
+| embed_author_icon_url     | ❌        | The embed's author's icon's url.                                      | `"https://picsum.photos/1080"`                                          | `null`  |
+| embed_image_url           | ❌        | The embed's image's url.                                              | `"https://picsum.photos/1080"`                                          | `null`  |
+| embed_thumbnail_url       | ❌        | The embed's thumbnail's url.                                          | `"https://picsum.photos/1080"`                                          | `null`  |
+| embed_footer_text         | ❌        | The embed's footer's text.                                            | `"DiscordLiveAnnouncer"`                                                | `null`  |
+| embed_footer_icon_url     | ❌        | The embed's footers's icon's url.                                     | `"https://picsum.photos/1080"`                                          | `null`  |
+| embed_fields_server       | ❌        | Turn on or off the 'Server' field in the embed.                       | `true`                                                                  | `true`  |
+| embed_fields_map          | ❌        | Turn on or off the 'Map' field in the embed.                          | `true`                                                                  | `true`  |
+| embed_fields_player_count | ❌        | Turn on or off the 'Player Count' field in the embed.                 | `true`                                                                  | `true`  |
+| embed_fields_player_list  | ❌        | Turn on or off the 'Player List' field in the embed.                  | `true`                                                                  | `true`  |
---
+++ discord-live-announcer.js
@@ -0,0 +1,198 @@
+import DiscordBasePlugin from "./discord-base-plugin.js";
+
+export default class DiscordLiveAnnouncer extends DiscordBasePlugin {
+  static get description() {
+    return "The <code>DiscordLiveAnnouncer</code> plugin will send a message to a Discord channel when the server goes live.";
+  }
+
+  static get defaultEnabled() {
+    return true;
+  }
+
+  static get optionsSpecification() {
+    return {
+      ...DiscordBasePlugin.optionsSpecification,
+      channelID: {
+        required: true,
+        description: "The ID of the channel to send the announcements to.",
+        example: "1283199362318602270"
+      },
+      seedingThreshold: {
+        required: true,
+        description: "Player count required for the server to be considered live.",
+        example: 50
+      },
+      content: {
+        required: false,
+        description: "The content of the discord message to send when the server goes live.",
+        default: null,
+        example: "<@&1041503735660163112>"
+      },
+      embed_title: {
+        required: false,
+        description: "The embed's title.",
+        default: null,
+        example: "Server is LIVE"
+      },
+      embed_url: {
+        required: false,
+        description: "The embed's url.",
+        default: null,
+        example: "https://github.com/ItsMax123/SquadJS-DiscordLiveAnnouncer"
+      },
+      embed_description: {
+        required: false,
+        description: "The embed's description.",
+        default: null,
+        example: "Thank you to all who helped seed the server. Now come join the fun!"
+      },
+      embed_color: {
+        required: false,
+        description: "The embed's color.",
+        default: 65280,
+        example: 65280
+      },
+      embed_author_name: {
+        required: false,
+        description: "The embed's author's name.",
+        default: null,
+        example: "DiscordLiveAnnouncer"
+      },
+      embed_author_url: {
+        required: false,
+        description: "The embed's author's url.",
+        default: null,
+        example: "https://github.com/ItsMax123/SquadJS-DiscordLiveAnnouncer"
+      },
+      embed_author_icon_url: {
+        required: false,
+        description: "The embed's author's icon's url.",
+        default: null,
+        example: "https://picsum.photos/1080"
+      },
+      embed_image_url: {
+        required: false,
+        description: "The embed's image's url.",
+        default: null,
+        example: "https://picsum.photos/1080"
+      },
+      embed_thumbnail_url: {
+        required: false,
+        description: "The embed's thumbnail's url.",
+        default: null,
+        example: "https://picsum.photos/1080"
+      },
+      embed_footer_text: {
+        required: false,
+        description: "The embed's footer's text.",
+        default: null,
+        example: "DiscordLiveAnnouncer"
+      },
+      embed_footer_icon_url: {
+        required: false,
+        description: "The embed's footers's icon's url.",
+        default: null,
+        example: "https://picsum.photos/1080"
+      },
+      embed_fields_server: {
+        required: false,
+        description: "Turn on or off the 'Server' field in the embed",
+        default: true,
+        example: true
+      },
+      embed_fields_map: {
+        required: false,
+        description: "Turn on or off the 'Map' field in the embed",
+        default: true,
+        example: true
+      },
+      embed_fields_player_count: {
+        required: false,
+        description: "Turn on or off the 'Player Count' field in the embed",
+        default: true,
+        example: true
+      },
+      embed_fields_player_list: {
+        required: false,
+        description: "Turn on or off the 'Player List' field in the embed",
+        default: true,
+        example: true
+      }
+    };
+  }
+
+  constructor(server, options, connectors) {
+    super(server, options, connectors);
+    this.sent = false;
+    this.onPlayerCountChange = this.onPlayerCountChange.bind(this);
+  }
+
+  async mount() {
+    await super.mount();
+    this.server.on("PLAYER_CONNECTED", this.onPlayerCountChange);
+  }
+
+  async unmount() {
+    await super.unmount();
+    this.server.removeEventListener("PLAYER_CONNECTED", this.onPlayerCountChange);
+  }
+
+  async onPlayerCountChange(info) {
+    const layer = await this.server.currentLayer;
+    if (!layer) {
+      this.verbose(1, "Failed to load current layer.");
+      return;
+    }
+    if (layer.gamemode === "Seed") {
+      const playerCount = this.server.players.length;
+      if (playerCount < this.options.seedingThreshold) return;
+      this.verbose(1, "2");
+      const message = {
+        content: this.options.content,
+        embed: {
+          title: this.options.embed_title,
+          url: this.options.embed_url,
+          description: this.options.embed_description,
+          color: this.options.embed_color,
+          author: {
+            name: this.options.embed_author_name,
+            url: this.options.embed_author_url,
+            icon_url: this.options.embed_author_icon_url
+          },
+          fields: [],
+          image: {
+            url: this.options.embed_image_url
+          },
+          thumbnail: {
+            url: this.options.embed_thumbnail_url
+          },
+          footer: {
+            text: this.options.embed_footer_text,
+            icon_url: this.options.embed_footer_icon_url
+          },
+          timestamp: info.time.toISOString()
+        }
+      };
+      if (this.options.embed_fields_server) message.embed.fields.push({
+        name: "Server",
+        value: this.server.serverName
+      });
+      if (this.options.embed_fields_map) message.embed.fields.push({
+        name: "Map",
+        value: layer.name
+      });
+      if (this.options.embed_fields_player_count) message.embed.fields.push({
+        name: "Player Count",
+        value: `${playerCount} / ${this.server.maxPlayers}`
+      });
+      if (this.options.embed_fields_player_list) message.embed.fields.push({
+        name: "Players",
+        value: `${this.server.players.map((obj) => obj.name).join("\n")}`
+      });
+      await this.sendDiscordMessage(message);
+      this.sent = true;
+    } else if (this.sent) {
+      this.sent = false;
+    }
+  }
+}
